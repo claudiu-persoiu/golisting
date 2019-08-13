9c5095b8d0265e502d404ace973fdfc9b7d04a7d
Claudiu Persoiu <claudiu@claudiupersoiu.ro>
2019-08-13 20:50:41 +0300

    added build file

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-GOOS=linux GOARCH=arm go build -o golisting.arm
-GOOS=linux GOARCH=amd64 go build -o golisting.amd64
+GOOS=linux GOARCH=arm go build -o bin/golisting-linux-arm
+GOOS=linux GOARCH=amd64 go build -o bin/golisting-linux-amd64
+GOOS=linux GOARCH=386 go build -o bin/golisting-linux-368
+
+GOOS=windows GOARCH=arm go build -o bin/golisting-win-arm
+GOOS=windows GOARCH=amd64 go build -o bin/golisting-win-amd64
+GOOS=windows GOARCH=386 go build -o bin/golisting-win-368
+
+GOOS=darwin GOARCH=amd64 go build -o bin/golisting-darwin-amd64
+GOOS=darwin GOARCH=386 go build -o bin/golisting-darwin-368
