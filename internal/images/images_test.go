af74f531f6763d28c025b74c447cd368169a8c0d
Claudiu Persoiu <claudiu.persoiu@happening.xyz>
2025-12-16 21:21:54 +0200

    added a first test

---
+++ internal/images/images_test.go
@@ -0,0 +1,47 @@
+package images
+
+import (
+	"io/fs"
+	"os"
+	"slices"
+	"testing"
+)
+
+type mockDirEntry struct {
+	name  string
+	isDir bool
+}
+
+func (m mockDirEntry) Name() string {
+	return m.name
+}
+
+func (m mockDirEntry) IsDir() bool {
+	return m.isDir
+}
+
+func (m mockDirEntry) Type() fs.FileMode {
+	panic("implement me")
+}
+
+func (m mockDirEntry) Info() (fs.FileInfo, error) {
+	panic("implement me")
+}
+
+func TestFilterAndSortImages(t *testing.T) {
+
+	input := []os.DirEntry{
+		mockDirEntry{name: "img10.png"},
+		mockDirEntry{name: "img2.jpg"},
+		mockDirEntry{name: "document.pdf"},
+		mockDirEntry{name: "document.jpg", isDir: true},
+	}
+
+	output := filterAndSortImages(input)
+
+	expected := []string{"img2.jpg", "img10.png"}
+
+	if !slices.Equal(expected, output) {
+		t.Errorf("Expected %v, but got %v", expected, output)
+	}
+}
