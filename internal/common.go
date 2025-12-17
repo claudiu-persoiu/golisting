6082235ee86378615ff5b2191e828d01e1985441
Claudiu Persoiu <claudiu.persoiu@happening.xyz>
2025-12-17 17:09:34 +0200

    added some more tests

--- internal/common.go
+++ internal/common.go
@@ -24,7 +24,20 @@ func OutputHTML(templateBox embed.FS, file string, pageData *PageData, wr io.Wri
 }
 
 func FileExists(path string) bool {
-	_ , err := os.Stat(path)
+	_, err := os.Stat(path)
 
 	return !os.IsNotExist(err)
+}
+
+func CreateDirIfNotExists(path string) error {
+	return createDirIfNotExistsSilent(path, FileExists, os.Mkdir)
+}
+
+func createDirIfNotExistsSilent(path string, exists func(string) bool, mkdir func(string, os.FileMode) error) error {
+	if !exists(path) {
+		if err := mkdir(path, os.ModePerm); err != nil {
+			return err
+		}
+	}
+	return nil
 }
