b97d244df6669a9448b8d7d895f5a6ef0bfc3696
Claudiu Persoiu <claudiu.persoiu@happening.xyz>
2025-12-18 10:53:40 +0200

    check images by mime type instead of file extensions

--- internal/images/images.go
+++ internal/images/images.go
@@ -2,6 +2,8 @@ package images
 
 import (
 	"log"
+	"log/slog"
+	"net/http"
 	"os"
 	"path/filepath"
 	"slices"
@@ -15,15 +17,13 @@ func GetImages(dir string) []string {
 		log.Fatal(err)
 	}
 
-	return filterAndSortImages(files)
+	return filterAndSortImages(files, dir, checkAllowedImageFormats)
 }
 
-func filterAndSortImages(files []os.DirEntry) []string {
-
+func filterAndSortImages(files []os.DirEntry, dir string, imageChecker func(string) bool) []string {
 	var images []string
 	for _, f := range files {
-		ext := filepath.Ext(f.Name())
-		if !f.IsDir() && (ext == ".jpg" || ext == ".png") {
+		if !f.IsDir() && imageChecker(filepath.Join(dir, f.Name())) {
 			images = append(images, f.Name())
 		}
 	}
@@ -43,4 +43,25 @@ func FilterOutImages(allImages, errorImages []string) []string {
 	return slices.DeleteFunc(allImages, func(image string) bool {
 		return slices.Contains(errorImages, image)
 	})
+}
+
+func checkAllowedImageFormats(image string) bool {
+	file, err := os.Open(image)
+	if err != nil {
+		slog.Info("Error opening file " + image + " error: " + err.Error())
+		return false
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+	if err != nil {
+		slog.Info("Error reading file " + image + " error: " + err.Error())
+		return false
+	}
+
+	contentType := http.DetectContentType(buffer)
+	allowedTypes := []string{"image/jpeg", "image/png"}
+
+	return slices.Contains(allowedTypes, contentType)
 }
